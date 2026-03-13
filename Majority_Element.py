f2ab45871bc7c937aa3118dddd022575db4fc479
doquocdat2702 <ddat2405@gmail.com>
2026-03-13 13:50:16 +0700

    first commit

---
+++ Majority_Element.py
@@ -0,0 +1,8 @@
+class Solution:
+    def majorityElement(self, nums):
+        count = {}
+
+        for num in nums:
+            count[num] = count.get(num, 0) + 1
+            if count[num] > len(nums) // 2:
+                return num
